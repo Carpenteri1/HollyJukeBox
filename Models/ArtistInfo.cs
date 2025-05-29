dd76ad683df7550cd6625045ea4443c91e8950b2
niclastimle <niclastimle@icloud.com>
2025-05-30 01:23:41 +0200

    fix nullable albums

--- Models/ArtistInfo.cs
+++ Models/ArtistInfo.cs
@@ -4,6 +4,6 @@ public class ArtistInfo
 {
     public required string Mbid { get; set; }
     public required string Artist { get; set; }
-    public required List<AlbumInfo>? Albums { get; set; }
+    public required List<AlbumInfo> Albums { get; set; }
     public required string Description { get; set; }
 }
